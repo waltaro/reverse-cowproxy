b1d548a0a0000c504c5014338d4654672d88b226
Walter Ngo <ngo.walter@gmail.com>
2017-04-18 16:31:21 +1000

    Scraper now abstracted into render function

--- renderer/renderer.js
+++ renderer/renderer.js
@@ -2,7 +2,38 @@ const CDP = require('chrome-remote-interface');
 const config = require('../config/config.js');
 
 function render(url) {
-  console.log('Rendering..');
+
+  CDP((client) => {
+    // Extract used DevTools domains.
+    const {
+      Page,
+      Runtime,
+    } = client;
+
+    // Evaluate outerHTML after page has loaded.
+    Page.loadEventFired(() => {
+      setTimeout(() => {
+        Runtime.evaluate({
+          expression: 'document.documentElement.outerHTML',
+        }).then((result) => {
+          return result.result.value;
+          client.close();
+        });
+      }, config.delay);
+    });
+
+    // Enable events on domains we are interested in.
+    Promise.all(
+      [Page.enable()]
+    ).then(() => {
+      return Page.navigate({
+        url: url,
+      });
+    });
+
+  }).on('error', (err) => {
+    console.error('[Reverse Cowproxy] : cannot connect to browser:', err);
+  });
 }
 
 module.exports = {
--- reverse-cowproxy.js
+++ reverse-cowproxy.js
@@ -18,38 +18,7 @@ cowproxy.get(config.endpoint, (req, res) => {
 cowproxy.post(config.endpoint, (req, res) => {
   // need to add error checking for url
   let url = req.body.url;;
-
-  CDP((client) => {
-    // Extract used DevTools domains.
-    const {
-      Page,
-      Runtime,
-    } = client;
-
-    // Evaluate outerHTML after page has loaded.
-    Page.loadEventFired(() => {
-      setTimeout(() => {
-        Runtime.evaluate({
-          expression: 'document.documentElement.outerHTML',
-        }).then((result) => {
-          res.send(result.result.value);
-          client.close();
-        });
-      }, config.delay);
-    });
-
-    // Enable events on domains we are interested in.
-    Promise.all(
-      [Page.enable()]
-    ).then(() => {
-      return Page.navigate({
-        url: url,
-      });
-    });
-
-  }).on('error', (err) => {
-    console.error('[Reverse Cowproxy] : cannot connect to browser:', err);
-  });
+  res.send(renderer.render(url));
 });
 
 cowproxy.listen(config.port, config.hostname, () => {
